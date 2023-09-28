36e4431ac0ed95888f5018d77152da87051fe82b
S <s>
2023-09-28 18:53:30 +0400

    {"types":"C","runs":10}

--- Problem02.java
+++ Problem02.java
@@ -1,4 +1,24 @@
+import stanford.karel.Karel;
 
-public class Problem02 {
-
+public class Problem02 extends Karel {
+//2. კარელი დგას 1x1 უჯრაზე. 5x1 უჯრაზე დევს 10 ცალი ბრილიანტი, კარელმა ათივე
+//	ბრილიანტი უნდა გადაიტანოს 6x1 უჯრაზე.
+	public void run(){
+		move();
+		move();
+		move();
+		move();
+		pickBeeper();
+		pickBeeper();
+		pickBeeper();
+		pickBeeper();
+		pickBeeper();
+		pickBeeper();
+		pickBeeper();
+		pickBeeper();
+		pickBeeper();
+		pickBeeper();
+		move();
+		
+	}
 }
