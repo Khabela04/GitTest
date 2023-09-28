4ff4860659bbe24664e9fd4ba3c6146d73b49c00
S <s>
2023-09-28 18:59:59 +0400

    {"types":"C","runs":14}

--- Problem02.java
+++ Problem02.java
@@ -4,10 +4,12 @@ public class Problem02 extends Karel {
 //2. კარელი დგას 1x1 უჯრაზე. 5x1 უჯრაზე დევს 10 ცალი ბრილიანტი, კარელმა ათივე
 //	ბრილიანტი უნდა გადაიტანოს 6x1 უჯრაზე.
 	public void run(){
+		
+		
+	for( int i=0; i<4; i++) {
 		move();
-		move();
-		move();
-		move();
+	}
+	
 		pickBeeper();
 		pickBeeper();
 		pickBeeper();
