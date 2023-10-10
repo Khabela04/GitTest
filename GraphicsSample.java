9a56f58625418947bfad09bd1a43e8b7c58e62f3
S <s>
2023-10-10 18:44:59 +0400

    {"types":"C","runs":9}

--- GraphicsSample.java
+++ GraphicsSample.java
@@ -10,7 +10,10 @@ public class GraphicsSample extends GraphicsProgram {
 		add(myRect, 100, 150);
 //		GRect secondRect = new GRect(100, 200);
 //		add(secondRect, 300, 100);
+		GRect oldRect = myRect();
 		myRect = new GRect(100, 200);
 		add(myRect, 300, 150);
+		oldRect.setFilled(true);
+		myRect.setFilled(true);
 	}
 }
