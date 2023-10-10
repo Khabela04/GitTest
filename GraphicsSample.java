2c9bf3f3900e70b85ecc035bd8b33bef663541fd
S <s>
2023-10-10 18:32:59 +0400

    {"types":"C","runs":5}

--- GraphicsSample.java
+++ GraphicsSample.java
@@ -3,8 +3,10 @@ import acm.program.GraphicsProgram;
 
 public class GraphicsSample extends GraphicsProgram {
 	public void run(){
+		
+//		add(new GRect(100, 200), 100, 150);
+//		add(new GOval(100, 200), 100, 150);
 		GRect myRect = new GRect(100, 200);
-		add(new GRect(100, 200), 100, 150);
-		add(new GOval(100, 200), 100, 150);
+		add(myRect, 100, 150);
 	}
 }
