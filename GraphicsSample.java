b15b896bc9000495d7862d6c0355d822708de514
S <s>
2023-10-10 18:30:17 +0400

    {"types":"C","runs":4}

--- GraphicsSample.java
+++ GraphicsSample.java
@@ -5,6 +5,6 @@ public class GraphicsSample extends GraphicsProgram {
 	public void run(){
 		GRect myRect = new GRect(100, 200);
 		add(new GRect(100, 200), 100, 150);
-		
+		add(new GOval(100, 200), 100, 150);
 	}
 }
