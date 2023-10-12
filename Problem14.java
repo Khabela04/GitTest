8154d07724e87189dd48ffb99065834bb783a04a
S <s>
2023-10-12 17:26:54 +0400

    {"types":"C","runs":10}

--- Problem14.java
+++ Problem14.java
@@ -1,8 +1,12 @@
+import acm.graphics.*;
 import acm.program.GraphicsProgram;
 //დახატეთ რობოტის სახე ფანჯრის ცენტრში.
 public class Problem14 extends GraphicsProgram{
 	public void run(){
-		
+		int height = getHeight();
+		int width = getWidth();
+		GRect Face = new GRect(100, 100);
+		add(Face, (height-100)/2, (width-100)/2);
 	}
 
 }
