60e4b2e8f52f56f9e3da77414bedee347c59eb6f
S <s>
2023-10-12 18:17:57 +0400

    {"types":"C","runs":1}

--- Problem012.java
+++ Problem012.java
@@ -1,4 +1,17 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
-public class Problem012 {
+public class Problem012 extends GraphicsProgram {
+	public void run(){
+		int width = 100;
+		width = width+20;
+		width = 200;
+		GRect rect = new GRect(width, width, width, width);
+		add(rect);
+		
+	}
+	private void testVoid(){
+		
+	}
 
 }
