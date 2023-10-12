4763fa4c1386dda6303616ebf883901d7ecba784
S <s>
2023-10-12 17:12:24 +0400

    {"types":"C","runs":1}

--- Problem13.java
+++ Problem13.java
@@ -1,4 +1,14 @@
+import java.awt.Dimension;
 
-public class Problem13 {
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+//დაწერეთ პროგრამა, რომლიც ეკრანის შუაში გაავლებს ჰორიზონტალურ ხაზს
+public class Problem13 extends GraphicsProgram {
+	public void run() {
+		GRect myRect = new GRect (80,160);
+		Dimension canvasSize = getSize();
+        int width = canvasSize.width;
+        int height = canvasSize.height;
+	}
 
 }
