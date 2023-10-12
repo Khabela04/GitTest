776f6035797532856d676e2562509e272e004fb2
S <s>
2023-10-12 17:28:48 +0400

    {"types":"C","runs":15}

--- Problem14.java
+++ Problem14.java
@@ -5,8 +5,8 @@ public class Problem14 extends GraphicsProgram{
 	public void run(){
 		int height = getHeight();
 		int width = getWidth();
-		GRect Face = new GRect(100, 100);
-		add(Face, (width-100)/2, (height-100)/2);
+		GRect Face = new GRect(200, 200);
+		add(Face, (width-200)/2, (height-200)/2);
 	}
 
 }
