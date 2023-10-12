cec2bc6cd463aee48fcf201e5d75868f0a15ac98
S <s>
2023-10-12 18:28:22 +0400

    {"types":"C","runs":2}

--- Problem012.java
+++ Problem012.java
@@ -2,16 +2,34 @@ import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
 public class Problem012 extends GraphicsProgram {
+	private static final int WIDTH = 200;
 	public void run(){
-		int width = 100;
-		width = width+20;
-		width = 200;
-		GRect rect = new GRect(width, width, width, width);
-		add(rect);
+		drawMidLine();
+	}
+	
+	private void drawMidLine(){
+		double x1 = 0;
+		double y1 = getHeight()/2;
+		double x2 = getWidth();
+		double y2 = getHeight()/2;
+		GLine line = new GLine(x1, y1, x2, y2);
+		add(line);
 		
 	}
-	private void testVoid(){
-		
-	}
+//		int width = 100;
+//		width = width+20;
+//		width = 200;
+//		GRect rect = new GRect(width, width, width, width);
+//		add(rect);
+//		if(true){
+//			width = 5;
+//			int height = 30;
+//		}
+//		
+//		
+//	}
+//	private void testVoid(){
+//		
+//	}
 
 }
