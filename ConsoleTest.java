c265d6b9056060f26b3d5e6f9d476ee818bd10cc
S <s>
2023-10-12 19:10:13 +0400

    {"types":"C","runs":23}

--- ConsoleTest.java
+++ ConsoleTest.java
@@ -4,6 +4,10 @@ public class ConsoleTest extends ConsoleProgram {
 	public void run(){
 	int kids = readInt("Enter number of your kids: ");
 	println("Your kid's number is"+kids);
+	int firstNum = readInt("Enter the first num: ");
+	int secondNum = readInt("Enter the first num: ");
+	int avg = firstNum+secondNum;
+	println("Average is "+avg);
 
 }
 }
