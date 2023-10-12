4573460478616b27e8d9a85ced7df1773ef1350a
S <s>
2023-10-12 19:08:10 +0400

    {"types":"C","runs":22}

--- ConsoleTest.java
+++ ConsoleTest.java
@@ -1,7 +1,9 @@
 import acm.program.ConsoleProgram;
 
 public class ConsoleTest extends ConsoleProgram {
+	public void run(){
 	int kids = readInt("Enter number of your kids: ");
-	
+	println("Your kid's number is"+kids);
 
+}
 }
