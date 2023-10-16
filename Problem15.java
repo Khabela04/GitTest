3b5ef58b3123f500bc7670ac57f2145933e9559e
S <s>
2023-10-16 20:43:48 +0400

    {"types":"C","runs":8}

--- Problem15.java
+++ Problem15.java
@@ -5,7 +5,7 @@ public class Problem15 extends ConsoleProgram {
 	public void run(){
 		int num1 = readInt("Write first whole number ");
 		int num2 = readInt("Write second whole number ");
-		double avg = (num1+num2)/2;
+		double avg = (num1+num2)/3;
 		println("Average of this two numbers is "+ avg);
 	}
 
