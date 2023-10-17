cff1f72841d112c3ab0666dba932639d0c625410
S <s>
2023-10-17 18:41:42 +0400

    {"types":"C","runs":5}

--- SeminarExtra6.java
+++ SeminarExtra6.java
@@ -1,4 +1,15 @@
+import acm.program.ConsoleProgram;
+//1. კონსოლიდან იკითხეთ რიცხვები, მანამ სანამ შემოსული რიცხვი არ იქნება 0. 0-ის
+//შემოსვლის შემდეგ დაბეჭდეთ შემოსული რიცხვების ჯამი.
 
-public class SeminarExtra6 {
+public class SeminarExtra6 extends ConsoleProgram {
+	private static final int GOLDEN_NUMBER = 0;
+	public void run(){
+		while(true){
+			int number = readInt("Give me a number");
+			if(number == GOLDEN_NUMBER) { break;}
+					number++;
+		}
+		}
 
 }
