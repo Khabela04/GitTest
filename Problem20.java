257aaee541781d7f4f19221b7542ed816d318745
S <s>
2023-10-18 19:15:38 +0400

    {"types":"C","runs":1}

--- Problem20.java
+++ Problem20.java
@@ -1,4 +1,15 @@
+import acm.program.ConsoleProgram;
 
-public class Problem20 {
+//20. კონსოლიდან წაიკითხეთ n, შემდეგ წაიკითხეთ n ცალი მთელი რიცხვი და დაბეჭდეთ ჯამი 
+public class Problem20 extends ConsoleProgram {
+	public void run(){
+		int quantity = readInt("Give me a quantity of number ");
+		int sum = 0;
+		for(int i = 0; i<quantity+1; i++){
+			int number = readInt("Give me number ");
+			sum += number;
+		}
+		println("Sum is"+sum);
+	}
 
 }
