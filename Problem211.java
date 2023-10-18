225b87bd253577aa53f19b69e035b22a9dc2170b
S <s>
2023-10-18 19:58:25 +0400

    {"types":"C","runs":26}

--- Problem211.java
+++ Problem211.java
@@ -1,4 +1,24 @@
+import acm.graphics.*;
+import acm.program.*;
 
-public class Problem211 {
-
+//21. ხაზებით ბადის დახატვა 10x10 ზე 
+public class Problem211 extends GraphicsProgram {
+	public void run(){
+		int n = 10;
+		int l = 0;
+		int m = 10;
+		int x = 0;
+		int y = 0;
+		double boxHeight = 400/n;
+		double boxWidth = 400/m;
+		for(int i = 0; i<n+1; i++){
+			add(new GLine(0, y, 400, y));
+			y += boxHeight;
+			for(int j = 0; j<m+1; j++){
+				add(new GLine(x, 0, x, 400 ));
+				x += boxWidth;
+			}
+			}
+		
+	}
 }
