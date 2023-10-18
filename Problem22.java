0c96ad70abb3d4a4f9201ac36f9816ee2bf5e92a
S <s>
2023-10-18 20:40:34 +0400

    {"types":"C","runs":45}

--- Problem22.java
+++ Problem22.java
@@ -4,10 +4,16 @@ import acm.program.*;
 //22. ჭადრაკის დაფის დახატვა 8x8 ზე 
 public class Problem22 extends GraphicsProgram {
 	public void run(){
-		add(new GRect(20,20), 0, 0);
+		int x = 0;
+		int y = 0;
+		for(int i = 0; i<4; i++){
+		add(new GRect(20,20), x, y);
 		GRect myRect = new GRect(20,20);
 		myRect.setFilled(true);
-		add(myRect,0+20, 0);
+		x += 20;
+		add(myRect,x, y);
+		x += 20;
 	
 	}
+}
 }
