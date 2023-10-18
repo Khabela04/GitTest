c0b5d13b35d4527422300d150438625cfb420c61
S <s>
2023-10-18 19:27:32 +0400

    {"types":"C","runs":2}

--- Problem21.java
+++ Problem21.java
@@ -1,4 +1,18 @@
+import acm.graphics.*;
+import acm.program.*;
 
-public class Problem21 {
+//21. ხაზებით ბადის დახატვა 10x10 ზე 
+public class Problem21 extends GraphicsProgram {
+	public void run(){
+		int n = 10;
+		int m = 10;
+		double boxHeight = getHeight()/n;
+		double boxWidth = getWidth()/m;
+		for(int i = 0; i<n; i++){
+			GRect myRect = new GRect(getWidth(), boxHeight);
+			add (myRect, 0, 0);
+	
+		}
+	}
 
 }
