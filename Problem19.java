f354d5e5cbd909d954999ef720a52ad75e8650e3
S <s>
2023-10-18 19:27:44 +0400

    {"types":"C","runs":3}

--- Problem19.java
+++ Problem19.java
@@ -2,12 +2,9 @@ import acm.program.ConsoleProgram;
 //19. კონსოლიდან წაიკითხეთ მთელი რიცხვი n და შემდეგ n ჯერ დაბეჭდეთ “hello ” + i, სადაც i  იცვლება 0 დან n-1 მდე.  
 public class Problem19 extends ConsoleProgram {
 	public void run(){
-		double n = readDouble("Give me a number");
+		int n = readInt("Give me a number");
 		for(int i=0; i<n; i++){
 			println("hello World"+i);
-		}
-		while(true){
-			println("hello World");
 		}
 	}
 }
