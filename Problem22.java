45216b273055b3cba943747047a09be1ffe58285
S <s>
2023-10-18 20:46:46 +0400

    {"types":"C","runs":48}

--- Problem22.java
+++ Problem22.java
@@ -18,9 +18,9 @@ public class Problem22 extends GraphicsProgram {
 	}
 		for(int i = 0; i<4; i++){
 			y +=20;
-			GRect myRect = new GRect(x-160,y);
+			GRect myRect = new GRect(20,20);
 			myRect.setFilled(true);
-			add(myRect,x, y);
+			add(myRect,x-160, y);
 			
 		
 		}
