808aeb3644e0a37e5413dd955ed8c78b5b987de9
S <s>
2023-10-18 20:24:20 +0400

    {"types":"C","runs":40}

--- Problem22.java
+++ Problem22.java
@@ -4,21 +4,12 @@ import acm.program.*;
 //22. ჭადრაკის დაფის დახატვა 8x8 ზე 
 public class Problem22 extends GraphicsProgram {
 	public void run(){
-		int size = 256;
 		int n = 8;
-		int m = 8;
+		int boxWidth = getWidth()/n;
+		int boxHeight = getHeight()/n;
 		int x = 0;
 		int y = 0;
-		double boxHeight = size/n;
-		double boxWidth = size/m;
-		for(int i = 0; i<n+1; i++){
-			add(new GLine(0, y, size, y));
-			y += boxHeight;
+		add(new GRect(boxWidth, boxHeight), x,y);
 		
-			for(int j = 0; j<m+1; j++){
-				add(new GLine(x, 0, x, y ));
-				x += boxWidth;
-			}
-		}
 	}
 }
