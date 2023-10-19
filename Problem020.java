0f46e0d7dd653e54ea9fc63561971817d606b21e
S <s>
2023-10-19 17:36:05 +0400

    {"types":"C","runs":12}

--- Problem020.java
+++ Problem020.java
@@ -5,7 +5,7 @@ public class Problem020 extends ConsoleProgram {
 		int sum = 0;
 		int n = readInt("Give me a quantity");
 		for(int i = 0; i < n; i++){
-			int number = readInt("Give me a number # " + i );
+			int number = readInt("Give me a number #" + i + " :" );
 			sum += number;
 			println(number);
 		}
