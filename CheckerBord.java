d5769aa00126f1adecee5ff699ec1c97a9dc0c86
S <s>
2023-10-19 15:40:52 +0400

    {"types":"C","runs":3}

--- CheckerBord.java
+++ CheckerBord.java
@@ -6,7 +6,7 @@ public class CheckerBord extends GraphicsProgram {
 		int rowsNum = 8;
 		int columnsNum = 8;
 		for(int i = 0; i < rowsNum; i++){
-			for(int j = 0; i < columnsNum; i++){
+			for(int j = 0; j < columnsNum; j++){
 				double x = j*getWidth()/columnsNum;
 				double y = i*getHeight()/rowsNum;
 				GRect myRect = new GRect(x, y, getWidth()/columnsNum, getHeight()/rowsNum);
