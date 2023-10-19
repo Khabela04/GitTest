36e806e260ba73fd2ace5425fc33be2d812e829b
S <s>
2023-10-19 15:44:40 +0400

    {"types":"C","runs":6}

--- CheckerBord.java
+++ CheckerBord.java
@@ -3,15 +3,15 @@ import acm.program.*;
 
 public class CheckerBord extends GraphicsProgram {
 	public void run(){
-		int rowsNum = 8;
-		int columnsNum = 8;
+		int rowsNum = 16;
+		int columnsNum = 16;
 		for(int i = 0; i < rowsNum; i++){
 			for(int j = 0; j < columnsNum; j++){
 				double x = j*getWidth()/columnsNum;
 				double y = i*getHeight()/rowsNum;
 				GRect myRect = new GRect(x, y, getWidth()/columnsNum, getHeight()/rowsNum);
 				add(myRect);
-				if((x+y)%2 != 0){
+				if((x+y)%2 = 0){
 					myRect.setFilled(true);
 				}
 			}
