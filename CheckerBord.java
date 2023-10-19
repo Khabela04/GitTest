0a7b1c4b7aa42fca38ca3812e387cd44da70425c
S <s>
2023-10-19 15:37:08 +0400

    {"types":"C","runs":1}

--- CheckerBord.java
+++ CheckerBord.java
@@ -1,4 +1,15 @@
+import acm.program.*;
 
-public class CheckerBord {
+public class CheckerBord extends GraphicsProgram {
+	public void run(){
+		int rowsNum = 8;
+		int columnsNum = 8;
+		for(int i = 0; i < rowsNum; i++){
+			for(int j = 0; i < columnsNum; i++){
+				GRect myRect = new GRect(x, y, getWidth()/columnsNum, getHeight()/rowsNum);
+				add(myRect);
+			}
+		}
+	}
 
 }
