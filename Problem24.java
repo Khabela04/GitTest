e99a1899151533e488707572a1c51000be8c0e8b
S <s>
2023-10-24 01:43:39 +0400

    {"types":"C","runs":12}

--- Problem24.java
+++ Problem24.java
@@ -4,16 +4,22 @@ import acm.program.ConsoleProgram;
 public class Problem24 extends ConsoleProgram {
 	public void run(){
 		int n = readInt("Give me a number: ");
-		println(n+" can be divided on "+divisions(n)+" numbers");
-	}
-	private int divisions(int n){
-		int divisions = 0;
+		int divisions = 0; 
 		for(int i = 0; i < n; i++){
 			if(n % i == 0){
 				divisions += 1;
 			}
 		}
-		return n;
-		
+		println(n+" can be divided on "+divisions+" numbers");
 	}
+//	private int divisions(int n){
+//		int divisions = 0;
+//		for(int i = 0; i < n; i++){
+//			if(n % i == 0){
+//				divisions += 1;
+//			}
+//		}
+//		return n;
+//		
+//	}
 }
