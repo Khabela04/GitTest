50e2557b32644f3929df70651db64bbab9e121aa
S <s>
2023-10-24 02:15:39 +0400

    {"types":"C","runs":17}

--- Problem25.java
+++ Problem25.java
@@ -1,20 +1,11 @@
 import acm.program.ConsoleProgram;
-
-//24. მომხმარებელს შეყავს მთელი რიცხვი n, პროგრამამ უნდა დაბეჭდოს n ის გამყოფების რაოდენობა 
+//25. მომხმარებელს შეჰყავს ორი რიცხვი თქვენმა პროგრამამ უნდა დაბეჭდოს ამ ორი რიცხვის უმცირესი საერთო ჯერადი. 
 public class Problem25 extends ConsoleProgram {
-	int divisions = 0;
 	public void run(){
-		int n = readInt("Give me a number: ");
-		println(n+" can be divided on "+divisions(n)+" numbers");
+		int n = readInt("Give me the first number: ");
+		int m = readInt("Give me the second number: ");
+		int mult = n*m;
 	}
-	private int divisions(int n){
-		for(int i = 0; i < n; i++){
-			if(n % i == 0){
-				divisions += 1;
-			}
-		}
-		return divisions;
-		
-	}
+
 }
 
