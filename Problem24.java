e51641e09d73792caff5ec1d81065927639b4136
S <s>
2023-10-24 01:37:44 +0400

    {"types":"C","runs":6}

--- Problem24.java
+++ Problem24.java
@@ -1,4 +1,19 @@
+import acm.program.ConsoleProgram;
 
-public class Problem24 {
-
+//24. მომხმარებელს შეყავს მთელი რიცხვი n, პროგრამამ უნდა დაბეჭდოს n ის გამყოფების რაოდენობა 
+public class Problem24 extends ConsoleProgram {
+	int divisions = 0;
+	public void run(){
+		int n = readInt("Give me a number: ");
+		println(n+" can be divided on "+divisions(n)+" numbers");
+	}
+	private int divisions(int n){
+		for(int i = 0; i < n; i++){
+			if(n % i == 0){
+				divisions += 1;
+			}
+		}
+		return divisions;
+		
+	}
 }
