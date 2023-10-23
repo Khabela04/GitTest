a0992add8181f666e2698d13d714811685ac21cd
S <s>
2023-10-24 02:16:10 +0400

    {"types":"C","runs":19}

--- Problem25.java
+++ Problem25.java
@@ -5,6 +5,7 @@ public class Problem25 extends ConsoleProgram {
 		int n = readInt("Give me the first number: ");
 		int m = readInt("Give me the second number: ");
 		int mult = n*m;
+		println(mult);
 	}
 
 }
