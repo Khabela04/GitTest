d27f024b56f793a2ee29a437e58e9232e924f106
S <s>
2023-10-24 17:41:33 +0400

    {"types":"C","runs":6}

--- Problem025.java
+++ Problem025.java
@@ -1,4 +1,19 @@
+import acm.program.ConsoleProgram;
 
-public class Problem025 {
-
+public class Problem025 extends ConsoleProgram {
+	public void run(){
+		int n = readInt("Enter n: ");
+		int m = readInt("Enter n: ");
+		int gcd = gcd(n, m);
+		println("GCD is " +gcd);
+	}
+	private int gcd(int n, int m){
+		for (int i = Math.min(m, n); i >= 1; i--){
+			if(n % i ==0 && m % i == 0){
+				return i;
+			}
+		}
+		return m;
+		
+	}
 }
