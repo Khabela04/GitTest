6c5654e4ef977f091a794341fcd5b0e1e4832946
S <s>
2023-10-24 17:28:46 +0400

    {"types":"C","runs":2}

--- Problem023.java
+++ Problem023.java
@@ -5,14 +5,24 @@ public class Problem023 extends ConsoleProgram {
 		int n = readInt("Enter n: ");
 		int sum = calculateSum(n);
 		println("sum of number 1...n is: " + sum);
+		println(numberOfDivisors(n));
 	}
 
 	private int calculateSum(int n) {
-		// TODO Auto-generated method stub
 		int sum = 0;
 		for(int i = 0; i <= n; i++){
 			sum += i;
 		}
 		return sum;
 	}
+	private int numberOfDivisors(int n){
+		int num = 0;
+		for(int i = 0; i <= n; i++){
+			if (n % i == 0){
+				num += 1;
+			}
+		}
+		return num;
+	}
+
 }
