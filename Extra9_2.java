f5516973ca08268ab7430c69edbd5ef2ee8bc88e
S <s>
2023-10-24 17:55:15 +0400

    {"types":"C","runs":9}

--- Extra9_2.java
+++ Extra9_2.java
@@ -1,4 +1,9 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
-public class Extra9_2 {
-
+public class Extra9_2 extends GraphicsProgram {
+	public void run(){
+		int x = 4;
+		add(new GLabel("x before method "+ x), 0, 20);
+	}
 }
