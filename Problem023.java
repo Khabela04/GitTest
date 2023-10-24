bb7d0415f33397d3c27d161b31659a2d0ec76be0
S <s>
2023-10-24 17:18:17 +0400

    {"types":"C","runs":1}

--- Problem023.java
+++ Problem023.java
@@ -1,4 +1,18 @@
+import acm.program.ConsoleProgram;
 
-public class Problem023 {
+public class Problem023 extends ConsoleProgram {
+	public void run(){
+		int n = readInt("Enter n: ");
+		int sum = calculateSum(n);
+		println("sum of number 1...n is: " + sum);
+	}
 
+	private int calculateSum(int n) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		for(int i = 0; i <= n; i++){
+			sum += i;
+		}
+		return sum;
+	}
 }
