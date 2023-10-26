71db5e2b02f5727df86437c5a8d937d59dee7ab3
S <s>
2023-10-26 18:33:14 +0400

    {"types":"C","runs":4}

--- Problem28.java
+++ Problem28.java
@@ -1,4 +1,17 @@
-
-public class Problem28 {
-
+import acm.program.ConsoleProgram;
+//28. მომხმარებელს შეყავს დადებითი რიცხვები, მანამ სანამ არ შეიყვანს -1 ს, დაბეჭდეთ
+//რაოდენობა რამდენი ლუწი რიცხვი შეყვანა მომხმარებელმა.
+public class Problem28 extends ConsoleProgram {
+	public static final int NUM_BER = -1;
+	public void run(){
+		int evenN = 0;
+		int n = readInt("Guess the number");
+	while(n != NUM_BER){
+		if(n % 2 == 0){
+			evenN += 1;
+		}
+		
+	}
+	println(evenN);
+	}	
 }
