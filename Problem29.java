e7851fef071b540fdbce8905e0c2a41db619429b
S <s>
2023-10-26 19:01:37 +0400

    {"types":"C","runs":16}

--- Problem29.java
+++ Problem29.java
@@ -1,4 +1,13 @@
+import acm.program.ConsoleProgram;
+//29. მოხმარებელს შეყავს მთელი რიცხვი n, პროგრამამ უნდა დაბეჭდოს ფიბონაჩის მიმდევრობის
+//მე n წევრი.
+public class Problem29 extends ConsoleProgram {
+	public void run(){
+		int n = readInt("Give me a number");
+		println("the " + n + "th number of Fibonachi is" + fibN(n));
+	}
 
-public class Problem29 {
-
+	private int fibN(int n) {
+		return n-2+n-1;
+	}
 }
