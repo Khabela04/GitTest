43193e3cfcce659da4e2a871dbd4b3038c2e02fc
S <s>
2023-10-26 19:01:38 +0400

    {"types":"C","runs":16}

--- Problem28.java
+++ Problem28.java
@@ -7,13 +7,11 @@ public class Problem28 extends ConsoleProgram {
 		int evenN = 0;
 		int n = readInt("Guess the number");
 	while(n != NUM_BER){
-		
 		int m = readInt("Guess the number");
 		if(n % 2 == 0){
 			evenN += 1;
 		}
 		n = m;
-		
 	}
 	println(evenN);
 	}	
