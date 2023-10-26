9bc4883c0facbcd0f7bab668dfd2161ab7fb703d
S <s>
2023-10-26 18:20:15 +0400

    {"types":"C","runs":1}

--- Probelm27.java
+++ Probelm27.java
@@ -1,4 +1,17 @@
+import acm.program.ConsoleProgram;
 
-public class Probelm27 {
+public class Probelm27 extends ConsoleProgram {
+	public void run(){
+		int n = readInt("Give me the number");
+		int m = readInt("Give me the power");
+		println(n + "^" + m + "is" + power(n,m));
+	}
 
+	private int power(int n, int m) {
+		int product = 1;
+		for(int i = 0; i<m; i++ ){
+			product *= n;
+		}
+		return product;
+	}
 }
