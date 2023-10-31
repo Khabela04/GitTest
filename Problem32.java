2a2b007c53a3dfde79ab631780e21880059f74a3
S <s>
2023-10-31 18:02:29 +0400

    {"types":"C","runs":17}

--- Problem32.java
+++ Problem32.java
@@ -1,4 +1,24 @@
+import acm.program.ConsoleProgram;
 
-public class Problem32 {
+public class Problem32 extends ConsoleProgram {
+	public void run(){
+		
+		int number = readInt("Give me a number");
+		for (int i = 1; i< 1000; i++){
+			if (isPrime(i)){
+				println(i);
+			
+		}
+		}
+	}
+		private boolean isPrime(int num){
+			for(int i = 2; i< Math.sqrt(num); i++){
+				if(num % i == 0){
+					return false;
+				}
+			}
+			return true;
+		}
+	
 
 }
