acc226928d8eda260f93c1181fea4116e94f94cf
S <s>
2023-10-31 17:14:53 +0400

    {"types":"C","runs":8}

--- Problem30.java
+++ Problem30.java
@@ -3,7 +3,7 @@ import acm.program.ConsoleProgram;
 public class Problem30 extends ConsoleProgram {
 	public void run(){
 	int num = readInt("Give me a number");
-	for(double i = 0; Math.pow(10,i)<num; i++){
+	for(double i = 1; Math.pow(10,i)<num; i++){
 	int sub = (int)(num/(Math.pow(10,i)));
 	double min = num-sub*Math.pow(10,i);
 	println(sub);
