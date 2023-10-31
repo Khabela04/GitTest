025c37a3acb96bb660cb27f43c65dabe88bce196
S <s>
2023-10-31 15:49:20 +0400

    {"types":"C","runs":0}

--- Problem30.java
+++ Problem30.java
@@ -1,4 +1,11 @@
-
-public class Problem30 {
-
+import acm.program.ConsoleProgram;
+//30. მომხმარებელს შეყავს მთელი რიცხვი, დაბეჭდეთ რიცხვი რომელიც მიიღება ციფრების შებრუნებით, 1256 -> 6521 
+public class Problem30 extends ConsoleProgram {
+	public void run(){
+	int num = readInt("Give me a number");
+	for(int i = 1; 10i<num; i++){
+	int sub = num/10;
+	int min = num-sub*10;
+}
+	}
 }
