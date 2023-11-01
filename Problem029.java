d0c78fbb5a467d2e47814bf43f9db119572ba7df
S <s>
2023-11-01 11:06:57 +0400

    {"types":"C","runs":2}

--- Problem029.java
+++ Problem029.java
@@ -12,7 +12,7 @@ public class Problem029 extends ConsoleProgram {
 		}
 		int prev2 = 0;
 		int prev1 = 1;
-		for(int i = 2; i<n; i++){
+		for(int i = 2; i<=n; i++){
 			int cur = prev2 + prev1;
 			prev2 = prev1;
 			prev1 = cur;
