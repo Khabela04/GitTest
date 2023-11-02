195eae23dc7879821090f2d8fadbc81edc61de0c
S <s>
2023-11-02 17:11:00 +0400

    {"types":"C","runs":1}

--- Problem032.java
+++ Problem032.java
@@ -12,7 +12,7 @@ public class Problem032 extends ConsoleProgram {
 		}
 	}
 	private boolean primeN(int n){
-		for(int i = 2; i<n-1; i++){
+		for(int i = 2; i <n-1; i++){
 			if(n % i == 0){
 				return false;
 			}
