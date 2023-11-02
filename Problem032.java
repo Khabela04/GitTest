981f8176d5f676749bff5db95a97398f39aad03a
S <s>
2023-11-02 16:48:15 +0400

    {"types":"C","runs":1}

--- Problem032.java
+++ Problem032.java
@@ -1,4 +1,17 @@
+import acm.program.ConsoleProgram;
 
-public class Problem032 {
-
+public class Problem032 extends ConsoleProgram {
+	public void run(){
+		int num = readInt("Give me a number");
+		boolean primeN = primeN(num);
+		println(primeN);
+	}
+	private boolean primeN(int n){
+		for(int i = 2; i<n-1; i++){
+			if(n % i == 0){
+				return false;
+			}
+		}
+		return true;
+	}
 }
