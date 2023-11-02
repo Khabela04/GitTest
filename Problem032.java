eb578ac1fd9b72a08af03276528b565631db87d7
S <s>
2023-11-02 16:52:07 +0400

    {"types":"C","runs":6}

--- Problem032.java
+++ Problem032.java
@@ -4,7 +4,9 @@ public class Problem032 extends ConsoleProgram {
 	public void run(){
 		int num = readInt("Give me a number");
 		boolean primeN = primeN(num);
-		println(primeN);
+		if(primeN){
+			println(num+"is a prime number");
+		};
 	}
 	private boolean primeN(int n){
 		for(int i = 2; i<n-1; i++){
