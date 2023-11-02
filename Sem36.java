ebebb7f67f6e08127c29ad9fa1bd5e38fe6f5141
S <s>
2023-11-02 17:57:11 +0400

    {"types":"C","runs":12}

--- Sem36.java
+++ Sem36.java
@@ -1,4 +1,37 @@
+//36. თქვენი ამოცანაა გააკეთოთ მონეტის აგდების სიმულაციები და დათვალოთ საშუალოდ რამდენჯერ უნდა ავაგდოთ მონეტა რათა 3-ჯერ ზედიზედ ამოვიდეს ბორჯღალო.
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class Sem36 {
-
+public class Sem36 extends ConsoleProgram {
+	private static final int NUM_EXPERIMENTS = 10000;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	public void run(){
+		double sumFlips = 0;
+		for(int i = 0; i < NUM_EXPERIMENTS; i++){
+			int currFlips = holdExperiment();
+			sumFlips += currFlips;
+		}
+		double avgFlips = sumFlips / NUM_EXPERIMENTS;
+		println(avgFlips);
+		
+		
+	}
+	private int holdExperiment() {
+		int numFlips = 0;
+		int numConsecutiveHead = 0;
+		while (true){
+			numFlips++;
+			boolean isHeads = rgen.nextBoolean();
+			if (isHeads){
+				numConsecutiveHead ++;
+				if(numConsecutiveHead == 3){
+					break;
+				}
+			}
+			else{
+				numConsecutiveHead = 0;
+			}
+		}
+		return numFlips;
+	}
 }
