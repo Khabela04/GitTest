bb4c1c2f075a7cd96f3faef28079f4d348cc6ffb
S <s>
2023-11-02 17:11:01 +0400

    {"types":"C","runs":1}

--- Problem34Sem.java
+++ Problem34Sem.java
@@ -1,4 +1,10 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class Problem34Sem {
-
+//34. კანვასზე დახატეთ 1000 შემთხვევითი რადიუსის მქონე შემთხვევითი ფერით გაფერადებული წრე, შემთხვევით კოორდინატებზე. 
+public class Problem34Sem extends ConsoleProgram {
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	public void run(){
+		println(rgen.nextInt());
+	}
 }
