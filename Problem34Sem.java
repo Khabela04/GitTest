dd1faf2609b33d77ebc979626868848e75213c94
S <s>
2023-11-02 17:27:27 +0400

    {"types":"C","runs":7}

--- Problem34Sem.java
+++ Problem34Sem.java
@@ -10,7 +10,7 @@ public class Problem34Sem extends GraphicsProgram {
 	private static final int MAX_RADIUS = 20;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	public void run(){
-		for(int i= 0; i<1000; i++){
+		for(int i= 0; i<1000000; i++){
 			addRandomCircle();
 		}
 //		int a = 5;
