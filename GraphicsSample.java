d2b48c695f828ddfae7977ca37fce4807a884039
S <s>
2023-11-07 17:36:55 +0400

    {"types":"C","runs":19}

--- GraphicsSample.java
+++ GraphicsSample.java
@@ -24,7 +24,7 @@ public class GraphicsSample extends GraphicsProgram {
 //		oldRect.setLocation(10,10);
 //		oldRect.move(100,100);
 		
-		int finalX = 200;
+		int finalX = 195;
 		while(myRect.getX() != finalX){
 			int vx = INITIAL_VELOCITY ;
 			int vy = 0;
