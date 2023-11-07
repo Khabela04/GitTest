5871974a866063cddeb9fdd75b8f06fa361eaa5a
S <s>
2023-11-07 17:54:10 +0400

    {"types":"C","runs":30}

--- Problem039.java
+++ Problem039.java
@@ -1,4 +1,14 @@
+import acm.graphics.GOval;
+import acm.program.ConsoleProgram;
+import acm.program.GraphicsProgram;
 
-public class Problem039 {
-
+public class Problem039 extends GraphicsProgram {
+	private static final int RADIUS = 50;
+	private static final int VELOCITY_X = 5;
+	private static final int PAUSE = 20;
+	public void run(){
+		GOval myOval = new GOval(2*RADIUS, 2*RADIUS);
+		add(myOval, 0, getHeight()/2);
+		myOval.move(VELOCITY_X, 0);
+	}
 }
