1b2e11ff7ef676719c7e292c90d53c30c2703a8f
S <s>
2023-11-07 15:56:35 +0400

    {"types":"C","runs":3}

--- Problem035.java
+++ Problem035.java
@@ -1,4 +1,23 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class Problem035 {
-
+public class Problem035 extends ConsoleProgram {
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	private static final int NUM_EXPERIMENTS = 1000;
+	public void run(){
+		int sumFlips = 0;
+		int curFlips = 0;
+		for( int i = 0; i < NUM_EXPERIMENTS; i++){
+			sumFlips ++;
+			if (isHeads()){
+				curFlips++;
+			}
+		}
+		double avg = curFlips/sumFlips;
+		println(avg);
+	}
+	private boolean isHeads(){
+		boolean heads = rgen.nextBoolean();
+		return heads;
+	}
 }
