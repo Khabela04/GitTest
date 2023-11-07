99ad86bf63a10cdaf10a80f1da82cd15960aab13
S <s>
2023-11-07 17:54:11 +0400

    {"types":"C","runs":30}

--- Problem39.java
+++ Problem39.java
@@ -1,4 +1,13 @@
+import java.awt.Component;
 
-public class Problem39 {
-
+import acm.graphics.GOval;
+import acm.program.ConsoleProgram;
+//დაწერეთ პროგრამა სადაც იქნება კანვასზე ბურთის მოძრაობა მარცხენა კედლის შუა წერტილიდან მარჯვენა კედლამდე. 
+public class Problem39 extends ConsoleProgram {
+	private static final int RADIUS = 50;
+	
+	public void run(){
+		GOval Myoval = new GOval (2*RADIUS, 2* RADIUS);
+		add(Myoval, 0, 100);
+	}
 }
