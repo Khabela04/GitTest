052ffcb773c6af337947370cc932d0d99bf2da29
S <s>
2023-11-07 17:29:42 +0400

    {"types":"C","runs":12}

--- GraphicsSample.java
+++ GraphicsSample.java
@@ -4,7 +4,7 @@ import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
 public class GraphicsSample extends GraphicsProgram {
-	private static final int PAUSE_MS = 5;
+	private static final int PAUSE_MS = 1000;
 	public void run(){
 		
 //		add(new GRect(100, 200), 100, 150);
