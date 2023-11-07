74786d82bbe2508a866f26ab5c1f7d98ed3fb221
S <s>
2023-11-07 16:08:49 +0400

    {"types":"C","runs":8}

--- Problem035.java
+++ Problem035.java
@@ -1,6 +1,6 @@
 import acm.program.ConsoleProgram;
 import acm.util.RandomGenerator;
-
+//35. თქვენი ამოცანაა გააკეთოთ მონეტის აგდების სიმულაციები და დათვალოთ საშუალოდ რამდენჯერ უნდა ავაგდოთ მონეტა რათა ამოვიდეს ბორჯღალო 
 public class Problem035 extends ConsoleProgram {
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	private static final int NUM_EXPERIMENTS = 1000;
@@ -8,16 +8,19 @@ public class Problem035 extends ConsoleProgram {
 		double sumFlips = 0;
 		double curFlips = 0;
 		for( int i = 0; i < NUM_EXPERIMENTS; i++){
-			sumFlips ++;
-			if (isHeads()){
+			sumFlips += experiments();
+		}
+		println(sumFlips/NUM_EXPERIMENTS);
+	}
+	private int experiments(){
+		int currFlips = 0;
+		while(true){
+			boolean isHeads = rgen.nextBoolean();
+			if(isHeads){
 				break;
 			}
+			currFlips++;
 		}
-		double avg = sumFlips/1000;
-		println(avg);
-	}
-	private boolean isHeads(){
-		boolean heads = rgen.nextBoolean();
-		return heads;
+		return currFlips;
 	}
 }
