c132bca7932ec2d42b9132f46fa53597577ca053
S <s>
2023-11-07 18:02:53 +0400

    {"types":"C","runs":52}

--- Problem039.java
+++ Problem039.java
@@ -5,7 +5,7 @@ import acm.program.GraphicsProgram;
 public class Problem039 extends GraphicsProgram {
 	private static final int RADIUS = 50;
 	private static final int VELOCITY_X = 2;
-	private static final int PAUSE = 10000;
+	private static final int PAUSE = 10;
 	public void run(){
 		GOval myOval = new GOval(2*RADIUS, 2*RADIUS);
 		add(myOval, 0, getHeight()/2-RADIUS);
