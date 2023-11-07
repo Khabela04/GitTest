4113ea74f6b550e14952edd964dac57b6c624d7a
S <s>
2023-11-07 18:04:31 +0400

    {"types":"C","runs":55}

--- Problem039.java
+++ Problem039.java
@@ -15,7 +15,7 @@ public class Problem039 extends GraphicsProgram {
 	}
 		while(myOval.getX()-2*RADIUS > 0){
 			pause(PAUSE);
-			myOval.move(VELOCITY_X, 0);
+			myOval.move(-VELOCITY_X, 0);
 		}
 	}
 }
