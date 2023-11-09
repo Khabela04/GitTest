aaf0029d91c5f14070db5ad1cbdea64dbe6bd121
S <s>
2023-11-09 17:36:42 +0400

    {"types":"C","runs":7}

--- GraphicsEventsSample.java
+++ GraphicsEventsSample.java
@@ -1,8 +1,9 @@
 
-
 import java.awt.Color;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
+import acm.graphics.GLabel;
 import acm.graphics.GObject;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
@@ -10,30 +11,38 @@ import acm.program.GraphicsProgram;
 public class GraphicsEventsSample extends GraphicsProgram {
 	private GRect rect;
 	private GRect rect2;
-	public void init(){
+	private GLabel label;
+	public void init() {
 		rect = new GRect(100, 100);
 		rect.setFilled(true);
 		add(rect);
-		
+
 		rect2 = new GRect(100, 100);
 		rect2.setFilled(true);
 		add(rect2, 200, 200);
-		
+		label = new GLabel("waiting for a key");
 		addMouseListeners();
+		addKeyListeners();
 	}
-	public void run(){
-		while(true){
+
+	public void run() {
+		while (true) {
 			pause(10);
 		}
 	}
+
 	@Override
-	public void mouseClicked(MouseEvent e){
-//		System.out.println("here");
+	public void mouseClicked(MouseEvent e) {
+		// System.out.println("here");
 		e.getX();
 		GObject obj = getElementAt(e.getX(), e.getY());
-		
-		if(obj != null){
+
+		if (obj != null) {
 			obj.setColor(Color.RED);
 		}
+	}
+	@Override
+	public void keyTyped(KeyEvent e){
+		label.setLabel("key typed" + e.getKeyChar());
 	}
 }
