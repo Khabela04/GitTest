251ede1ef3e1651fa2b00d1eb0542639ed3be9f2
S <s>
2023-11-09 17:31:34 +0400

    {"types":"C","runs":6}

--- GraphicsEventsSample.java
+++ GraphicsEventsSample.java
@@ -8,20 +8,27 @@ import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class GraphicsEventsSample extends GraphicsProgram {
-	public void run(){
-		GRect rect = new GRect(100, 100);
+	private GRect rect;
+	private GRect rect2;
+	public void init(){
+		rect = new GRect(100, 100);
 		rect.setFilled(true);
 		add(rect);
 		
-		GRect rect2 = new GRect(100, 100);
+		rect2 = new GRect(100, 100);
 		rect2.setFilled(true);
 		add(rect2, 200, 200);
 		
 		addMouseListeners();
 	}
-	
+	public void run(){
+		while(true){
+			pause(10);
+		}
+	}
+	@Override
 	public void mouseClicked(MouseEvent e){
-		System.out.println("here");
+//		System.out.println("here");
 		e.getX();
 		GObject obj = getElementAt(e.getX(), e.getY());
 		
