b4d095d05d90725eb47c869dd59c4c3156c5d4a4
S <s>
2023-11-09 14:26:13 +0400

    {"types":"C","runs":0}

--- Problem039.java
+++ Problem039.java
@@ -10,6 +10,7 @@ public class Problem039 extends GraphicsProgram {
 	private static final int VELOCITY_X = 2;
 	private static final int PAUSE = 2;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
+
 	public void run() {
 		GOval myOval = new GOval(2 * RADIUS, 2 * RADIUS);
 		add(myOval, 0, getHeight() / 2 - RADIUS);
@@ -18,35 +19,34 @@ public class Problem039 extends GraphicsProgram {
 		while (myOval.getX() + 2 * RADIUS <= getWidth()) {
 			pause(PAUSE);
 			myOval.move(VELOCITY_X, 0);
-//			for (int j = 0; j < 100; j++) {
-//				GOval Oval = new GOval(2 * RADIUS, 2 * RADIUS);
-//				int y = rgen.nextInt(0, getHeight()- 2*RADIUS);
-//				add(Oval, 0, y);
-//				int vx = rgen.nextInt();
-//				Oval.move(vx, 0);
-//				Oval.setFilled(true);
-//				Oval.setColor(Color.BLUE);
-			
-			}
-		
-			while (myOval.getX() + 2 * RADIUS <= getWidth()) {
-				pause(PAUSE);
-				myOval.move(VELOCITY_X, 0);
-				for (int j = 0; j < 100; j++) {
-					GOval Oval = new GOval(2 * RADIUS, 2 * RADIUS);
-					int y = rgen.nextInt(0, getHeight()- 2*RADIUS);
-					add(Oval, 0, y);
-					int vx = rgen.nextInt();
-					Oval.move(vx, 0);
-					Oval.setFilled(true);
-					Oval.setColor(Color.BLUE);
-				
-				}
-			}
-			while (myOval.getX() > 0) {
-				pause(PAUSE);
-				myOval.move(-VELOCITY_X, 0);
+			// for (int j = 0; j < 100; j++) {
+			// GOval Oval = new GOval(2 * RADIUS, 2 * RADIUS);
+			// int y = rgen.nextInt(0, getHeight()- 2*RADIUS);
+			// add(Oval, 0, y);
+			// int vx = rgen.nextInt();
+			// Oval.move(vx, 0);
+			// Oval.setFilled(true);
+			// Oval.setColor(Color.BLUE);
+
+		}
+
+		while (myOval.getX() + 2 * RADIUS <= getWidth()) {
+			pause(PAUSE);
+			myOval.move(VELOCITY_X, 0);
+			for (int j = 0; j < 100; j++) {
+				GOval Oval = new GOval(2 * RADIUS, 2 * RADIUS);
+				int y = rgen.nextInt(0, getHeight() - 2 * RADIUS);
+				add(Oval, 0, y);
+				int vx = rgen.nextInt();
+				Oval.move(vx, 0);
+				Oval.setFilled(true);
+				Oval.setColor(Color.BLUE);
+
 			}
 		}
+		while (myOval.getX() > 0) {
+			pause(PAUSE);
+			myOval.move(-VELOCITY_X, 0);
+		}
 	}
-
+}
