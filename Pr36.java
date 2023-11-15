28f66a442407044c6c151bfeee454c4a2119181a
S <s>
2023-11-15 23:34:07 +0400

    {"types":"C","runs":31}

--- Pr36.java
+++ Pr36.java
@@ -1,4 +1,33 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class Pr36 {
+public class Pr36 extends ConsoleProgram {
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	int numFlips = 0;
+	int curFlips;
+	int sumFlips;
+	public void run(){
+		tossACoin();
+		for(int i = 0; i < 10000; i++){
+			sumFlips += tossACoin();
+		}
+		double avg = (double)(sumFlips)/10000;
+		println(avg);
+	}
+	private int tossACoin(){
+		numFlips = 0;
+		while(true){
+		boolean revSide = rgen.nextBoolean();
+		numFlips ++;
+		if(revSide){
+			revSide = rgen.nextBoolean();
+			if(revSide){
+				revSide = rgen.nextBoolean();
+				break;
+			}
+		}
+	}
+			return numFlips;
+		}
+	}
 
-}
