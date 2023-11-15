06dcf4c234a201dc41f85bfce4a2c28917f6be51
S <s>
2023-11-15 22:53:22 +0400

    {"types":"C","runs":12}

--- Pr34.java
+++ Pr34.java
@@ -6,10 +6,15 @@ public class Pr34 extends GraphicsProgram {
 	private static final int MIN_RADIUS = 40;
 	private static final int MAX_RADIUS = 80;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
+	int radius;
 	GOval o;
 	public void run(){
 		for(int i = 0; i < 1000; i++){
-			o = new GOval(rgen.nextInt(2 * MIN_RADIUS, 2 * MAX_RADIUS), rgen.nextInt(2 * MIN_RADIUS, 2 * MAX_RADIUS));
+			int radius = rgen.nextInt(MIN_RADIUS, MAX_RADIUS);
+			o = new GOval(2 * radius, 2 * radius);
+			o.setFilled(true);
+			o.setColor(rgen.nextColor());
+			add(o, rgen.nextDouble(0, getWidth() - 2 * radius), rgen.nextDouble(0, getHeight() - 2 * radius));
 		}
 	}
 }
