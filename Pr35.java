96ff88f527509b06a523e38df170bb26712c0e60
S <s>
2023-11-15 23:14:18 +0400

    {"types":"C","runs":13}

--- Pr35.java
+++ Pr35.java
@@ -1,4 +1,26 @@
-
-public class Pr35 {
-
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
+//35. თქვენი ამოცანაა გააკეთოთ მონეტის აგდების სიმულაციები და დათვალოთ საშუალოდ რამდენჯერ უნდა ავაგდოთ მონეტა რათა ამოვიდეს ბორჯღალო 
+public class Pr35 extends ConsoleProgram {
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	int numFlips;
+	public void run(){
+		tossACoin();
+		for(int i = 0; i < 1000; i++){
+			tossACoin();
+		}
+		int avg = numFlips/1000;
+	}
+	private int tossACoin(){
+		while(true){
+		boolean revSide = rgen.nextBoolean();
+		if(!revSide){
+			numFlips ++;
+		}
+		else{
+			return numFlips;
+		}
+		return numFlips;
+	}
+	}
 }
