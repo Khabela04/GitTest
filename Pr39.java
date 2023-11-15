6dea299066e9271ae4672dd63b757f6e3ed05503
S <s>
2023-11-15 23:53:43 +0400

    {"types":"C","runs":42}

--- Pr39.java
+++ Pr39.java
@@ -1,4 +1,24 @@
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
-public class Pr39 {
-
+//39. დაწერეთ პროგრამა სადაც იქნება კანვასზე ბურთის მოძრაობა მარცხენა კედლის შუა წერტილიდან მარჯვენა კედლამდე. 
+public class Pr39 extends GraphicsProgram {
+	private static final int DELAY = 10;
+	private static final int MIN_RADIUS = 20;
+	private static final int MAX_RADIUS = 40;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	GOval ball;
+	public void run(){
+		addBall();
+//		moveBall();
+		pause(DELAY);
+	}
+	private void addBall(){
+		double radius = rgen.nextDouble(MIN_RADIUS, MAX_RADIUS);
+		ball = new GOval(2 * radius, 2 * radius);
+		ball.setFilled(true);
+		ball.setColor(rgen.nextColor());
+		add(ball, 0, (getHeight()- 2 * radius)/2);
+	}
 }
