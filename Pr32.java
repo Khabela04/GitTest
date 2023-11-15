5846ef990314e3de11a5282cb2b09d59f248f50a
S <s>
2023-11-15 22:20:13 +0400

    {"types":"C","runs":2}

--- Pr32.java
+++ Pr32.java
@@ -2,16 +2,15 @@ import acm.program.ConsoleProgram;
 //32. დაწერეთ პროგრამა რომელიც იპოვის და დაბეჭდავს 1000000 მდე ყველა მერტივ რიცხვს 
 public class Pr32 extends ConsoleProgram {
 	private static final int NUM = 1000000;
+	private static final int EASY_NUM = 0;
 	public void run(){
 		for(int i = 0; i < NUM; i++){
 			for(int j = 2; j < Math.sqrt(NUM); j++){
 				if(i % j == 0){
 					break;
 				}
-				else{
-					println(i);
-				}
 			}
+			println(i);
 		}
 	}
 }
