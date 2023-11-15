16b38ddacaf7b890806c8636956397af0550220c
S <s>
2023-11-15 23:57:26 +0400

    {"types":"C","runs":43}

--- Pr39.java
+++ Pr39.java
@@ -7,18 +7,26 @@ public class Pr39 extends GraphicsProgram {
 	private static final int DELAY = 10;
 	private static final int MIN_RADIUS = 20;
 	private static final int MAX_RADIUS = 40;
+	private int V_X;
+	double radius;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	GOval ball;
 	public void run(){
 		addBall();
-//		moveBall();
+		moveBall();
 		pause(DELAY);
 	}
 	private void addBall(){
-		double radius = rgen.nextDouble(MIN_RADIUS, MAX_RADIUS);
+		radius = rgen.nextDouble(MIN_RADIUS, MAX_RADIUS);
 		ball = new GOval(2 * radius, 2 * radius);
 		ball.setFilled(true);
 		ball.setColor(rgen.nextColor());
 		add(ball, 0, (getHeight()- 2 * radius)/2);
+	}
+	private void moveBall(){
+		V_X = rgen.nextInt(2,10);
+		while(ball.getX() + 2 * radius< getWidth()){
+			ball.move(V_X, 0);
+		}
 	}
 }
