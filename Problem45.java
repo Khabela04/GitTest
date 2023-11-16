299b71cd0d186cdb631df2a136d678a957c97de5
S <s>
2023-11-16 16:44:07 +0400

    {"types":"C","runs":1}

--- Problem45.java
+++ Problem45.java
@@ -1,4 +1,16 @@
+import acm.program.ConsoleProgram;
 
-public class Problem45 {
-
+// 45. მომხმარებელს კონსოლიდან შეჰყავს ტექსტი და სიმბოლო, თქვენმა პროგრამამ უნდა იპოვოს რამდენჯერ გვხვდება მოცემულ ტექსტში მოცემული სიმბოლო 
+public class Problem45 extends ConsoleProgram {
+	int counter;
+	public void run(){
+		String text = readLine("Give me a text: ");
+		char symb = readLine("Give me a symbol: ");
+		for(int i = 0; i < text.length(); i++){
+			char charect = text.charAt(i);
+			if(charect == symb){
+				counter++;
+			}
+		}
+	}
 }
