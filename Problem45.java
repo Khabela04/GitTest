4ead89ef79ce7e2b1ed673e710a34704c73113e5
S <s>
2023-11-16 17:23:34 +0400

    {"types":"C","runs":4}

--- Problem45.java
+++ Problem45.java
@@ -4,8 +4,14 @@ import acm.program.ConsoleProgram;
 public class Problem45 extends ConsoleProgram {
 	int counter;
 	public void run(){
-		
-			
+		String text = readLine("Give me a text: ");
+		String symb = readLine("Give me a symbol: ");
+		char symbol = symb.charAt(0);
+		for(int i = 0; i < text.length(); i++){
+			if(symbol == i){
+				counter++;
+			}
+		}
 	}
 }
 
