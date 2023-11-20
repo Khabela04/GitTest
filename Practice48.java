31618270e1b562e43dba48ca2707fa4c078548e3
S <s>
2023-11-20 14:36:21 +0400

    {"types":"C","runs":57}

--- Practice48.java
+++ Practice48.java
@@ -12,7 +12,7 @@ public class Practice48 extends ConsoleProgram {
 		for(int i = 0; i < input.length(); i++){
 			char charecter1 = input.charAt(i);
 			int n = charrecter(input, charecter1);
-			println("charecter1 n");
+			println(charecter1 + n);
 		}
 		
 		return null;
