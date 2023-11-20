8765aa06ba3f80fb59c64ea502cd3ff04593ea7a
S <s>
2023-11-20 13:06:19 +0400

    {"types":"C","runs":15}

--- Prob42.java
+++ Prob42.java
@@ -1,4 +1,18 @@
+import java.awt.event.MouseEvent;
 
-public class Prob42 {
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
 
+//42. Paint-ის მონაკვეთის ხატვის გაკეთება, ჯერ აჭერ მაუსს, ფიქსირდება ერთი წვერო, 
+//მერე მაუსის მოძრაობისას მონაკვეთიც მოძრაობს რადგან მეორე წვერო არაა დაფიქსირებული და კლიკით ფიქსირდება მეორე წვეროც. 
+public class Prob42 extends GraphicsProgram {
+	private GLine line;
+	public void run(){
+		addMouseListeners();
+	}
+	public void mouseClicked(MouseEvent e){
+		line = new GLine(e.getX(), e.getY(), e.getX(), e.getY());
+		add(line);
+		line.setEndPoint(e.getX(), e.getY());
+	}
 }
