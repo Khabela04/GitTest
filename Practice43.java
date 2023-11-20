a741d6bde04a69e5052bb9765d4e0ce6effaad63
S <s>
2023-11-20 13:42:50 +0400

    {"types":"C","runs":35}

--- Practice43.java
+++ Practice43.java
@@ -16,6 +16,6 @@ public class Practice43 extends GraphicsProgram {
 		add(oval, (getWidth() - 2 * RADIUS) / 2, (getHeight() - 2 * RADIUS) / 2); 
 	}
 	public void mouseDragged(MouseEvent e){
-		oval.setLocation(oval.getX() - RADIUS, oval.getY() - RADIUS );
+		oval.setLocation(e.getX() - RADIUS, e.getY() - RADIUS );
 	}
 }
