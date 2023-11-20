e075dcc583a19f75762ec47fc366b9d1ad830f58
S <s>
2023-11-20 13:22:09 +0400

    {"types":"C","runs":21}

--- Practice43.java
+++ Practice43.java
@@ -1,4 +1,21 @@
+import java.awt.event.MouseEvent;
 
-public class Practice43 {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
+//43. წრეწირის drag ით გადაადგილება 
+public class Practice43 extends GraphicsProgram {
+	private static final int RADIUS = 5;
+	GOval oval;
+	public void run(){
+		createOval();
+		addMouseListeners();
+	}
+	private void createOval() {
+		oval = new GOval(2 * RADIUS, 2 * RADIUS);
+		add(oval, (getWidth() - 2 * RADIUS) / 2, (getHeight() - 2 * RADIUS) / 2); 
+	}
+	public void mouseDragged(MouseEvent e){
+		oval.move(e.getX() - oval.getX(), e.getY() - oval.getY());
+	}
 }
