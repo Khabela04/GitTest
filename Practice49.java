c93b0cb36ad477f76ac2bb72b4b18ee946d66b5d
S <s>
2023-11-20 16:48:51 +0400

    {"types":"C","runs":1}

--- Practice49.java
+++ Practice49.java
@@ -1,4 +1,20 @@
+import acm.program.ConsoleProgram;
 
-public class Practice49 {
+//49. დაწერეთ stringToInteger მეთოდი, რომელსაც გადაეცემა სტრინგი და აბრუნებს მთელ რიცხვს.  
+//ჩათვალეთ, რომ გადაცემული სტრინგი მხოლოდ ციფრებისგან შედგება და არ იწყება 0-ით.  მაგალითად “234” გადაცემის შემთხვევაში მეთოდმა უნდა დააბრუნოს 234.
+//ასევე ჩათვალეთ,  რომ დაბრუნებული მნიშვნელობა დადებითია და ეტევა int-ში. 
+public class Practice49 extends ConsoleProgram {
+	public void run(){
+		String txt = readLine("Give me a number: ");
+		int num = stringToInteger(txt);
+	}
 
+	private int stringToInteger(String txt) {
+		for(int i = 0; i < txt.length(); i++){
+			char charecter = txt.charAt(i);
+			int n = (int)(charecter);
+			return n;
+		}
+		return 0;
+	}
 }
