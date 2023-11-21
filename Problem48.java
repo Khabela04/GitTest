172cb8b03a5e767c6cc606c451d078e53341e939
S <s>
2023-11-21 17:22:31 +0400

    {"types":"C","runs":7}

--- Problem48.java
+++ Problem48.java
@@ -1,4 +1,26 @@
+import acm.program.ConsoleProgram;
 
-public class Problem48 {
+public class Problem48 extends ConsoleProgram {
+	int counter;
+	public void run(){
+		String text = "mariami";
+		printCharStatistics(text);
+	}
 
+	private void printCharStatistics(String text) {
+		for(int i = 0; i < text.length(); i++){
+			int count = counter(text, text.charAt(i));
+			print(text.charAt(i) + "is used " + count);
+		}
+		
+	}
+	private int counter(String text, char symbol){
+		for (int i = 0; i < text.length(); i++) {
+			char index = text.charAt(i);
+			if (symbol == index) {
+				counter++;
+			}
+		}
+		return counter;
+	}
 }
