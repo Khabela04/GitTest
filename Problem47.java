fd5ca1e850c4ff25707de16a765e5d98d8f06e5e
S <s>
2023-11-21 17:13:56 +0400

    {"types":"C","runs":3}

--- Problem47.java
+++ Problem47.java
@@ -3,13 +3,13 @@ import acm.program.ConsoleProgram;
 //47. დაწერეთ პრედიკატ მეთოდი რომელსაც გადაეცემა სტრინგი და რომელიც დაადგენს არის თუ არა მოცემული ტექსტი პალინდრომი 
 public class Problem47 extends ConsoleProgram {
 	public void run(){
-		String text = "mariami";
+		String text = "rogor";
 		boolean isPalindrom = isPalindrom(text);
 		println(text + " is palindrome: " + isPalindrom);
 	}
 
 	private boolean isPalindrom(String text) {
-		for(int i = 0; i < text.length()/2; i++){
+		for(int i = 0; i < (text.length()/2); i++){
 			if(text.charAt(i) == text.charAt(text.length() - i - 1)){
 				return false;
 			}
