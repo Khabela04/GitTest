a5156ccd392a4db6e0c056d84e393d8430d72447
S <s>
2023-11-21 17:58:39 +0400

    {"types":"C","runs":21}

--- tokenizerExample.java
+++ tokenizerExample.java
@@ -4,7 +4,7 @@ import acm.program.ConsoleProgram;
 
 public class tokenizerExample extends ConsoleProgram {
 	public void run(){
-		String name = "Saba /n2 Magaria";
+		String name = "Saba \n Magaria";
 		StringTokenizer tokenizer = new StringTokenizer(name);
 		while(tokenizer.hasMoreTokens()){
 			println(tokenizer.nextToken());
