7efd54a641dbc8b6e6a73334282c9c6a1f385b06
S <s>
2023-11-23 22:14:53 +0400

    {"types":"C","runs":4}

--- Practice51.java
+++ Practice51.java
@@ -4,16 +4,29 @@ import java.util.StringTokenizer;
 import acm.program.ConsoleProgram;
 
 public class Practice51 extends ConsoleProgram {
+	public int counter;
 	public void run(){
 		String text = readLine("Give me a text: ");
 		for(int i = 0; i < text.length(); i++){
 			char charecter = text.charAt(i);
 			String myText = "" + charecter;
+			if(charecterChecker(charecter, text)<1){
 			StringTokenizer tokenizer = new StringTokenizer(text, myText);
 			while(tokenizer.hasMoreTokens()){
 				println(tokenizer.nextToken());
 			}
 		}
+		}
+		
+	}
+
+	private int charecterChecker(char charecter, String text) {
+		for(int i = 0; i < text.length(); i++){
+			if(text.charAt(i) == charecter){
+				counter++;
+			}
+		}
+		return counter;
 		
 	}
 }
