e66a8ec5f721ccf08ce5ea37df7345056dfcfae4
S <s>
2023-11-23 18:00:13 +0400

    {"types":"A","runs":0}

---
+++ Problem14.java
@@ -0,0 +1,23 @@
+import java.awt.Color;
+
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+//დახატეთ რობოტის სახე ფანჯრის ცენტრში.
+public class Problem14 extends GraphicsProgram{
+	public void run(){
+		int height = getHeight();
+		int width = getWidth();
+		GRect Face = new GRect(200, 200);
+		add(Face, (width-200)/2, (height-200)/2);
+		add(new GRect(50, 50), (width-150)/2, (height-150)/2);
+		add(new GRect(50, 50), (width/2)+22, (height-150)/2);
+		add(new GLine(((width-150)/2)+50, (height-200), (width/2)+22, (height-200)));
+		add(new GLine((width/2)+22, height-200, (width/2)+22, height-170));
+		add(new GLine(((width-150)/2)+50, (height-200), ((width-150)/2)+50, height-170));
+		add(new GLine(((width-150)/2)+50, height-170,(width/2)+22, height-170 ));
+		Face.setColor(Color.GRAY);
+		Face.setFilled(true);
+		
+	}
+
+}
---
+++ Problem45.java
@@ -0,0 +1,19 @@
+import acm.program.ConsoleProgram;
+
+// 45. მომხმარებელს კონსოლიდან შეჰყავს ტექსტი და სიმბოლო, თქვენმა პროგრამამ უნდა იპოვოს რამდენჯერ გვხვდება მოცემულ ტექსტში მოცემული სიმბოლო 
+public class Problem45 extends ConsoleProgram {
+	int counter;
+
+	public void run() {
+		String text = readLine("Give me a text: ");
+		String symb = readLine("Give me a symbol: ");
+		char symbol = symb.charAt(0);
+		for (int i = 0; i < text.length(); i++) {
+			char index = text.charAt(i);
+			if (symbol == index) {
+				counter++;
+			}
+		}
+		println("This symbol is used for " + counter + " time");
+	}
+}
