b82f5472f33bc20fb04d45fdcdec8814c86266ff
S <s>
2023-11-23 18:59:54 +0400

    {"types":"C","runs":20}

--- Practice51.java
+++ Practice51.java
@@ -6,7 +6,7 @@ import acm.program.ConsoleProgram;
 public class Practice51 extends ConsoleProgram {
 	public void run(){
 		String text = readLine("Give me a text: ");
-		StringTokenizer tokenizer = new StringTokenizer(text, "a", true);
+		StringTokenizer tokenizer = new StringTokenizer(text, "a");
 		while(tokenizer.hasMoreTokens()){
 			print(tokenizer.nextToken());
 		}
