af155c7f0fee15c0473545061730da4d14ca0722
S <s>
2023-11-23 19:42:13 +0400

    {"types":"C","runs":23}

--- Practice52.java
+++ Practice52.java
@@ -1,4 +1,17 @@
+import acm.program.ConsoleProgram;
 
-public class Practice52 {
-
-}
+//52. დაწერეთ პროგრამა რომელიც წაიკითხავს თავის თავს ფაილიდან და დაბეჭდავს კონსოლში 
+public class Practice52 extends ConsoleProgram{
+	public void run(){
+	}
+	
+	}
+//		printer();
+//	}
+//
+//	private void printer() {
+//		println(printer);
+//		
+//	}
+	
+//}
