bd6ef8c25a4ac6f7ecb26aefc8b62e02a8c338dc
S <s>
2023-11-23 22:31:14 +0400

    {"types":"C","runs":11}

--- Practice51.java
+++ Practice51.java
@@ -7,14 +7,9 @@ public class Practice51 extends ConsoleProgram {
 	public void run(){
 		String text = readLine("Give me a text: ");
 		for(int i = 0; i < text.length(); i++){
-			StringTokenizer tokenizer = new StringTokenizer(text, "" + text.charAt(i));
+			StringTokenizer tokenizer = new StringTokenizer(text, "" + text.charAt(i+1));
 			while(tokenizer.hasMoreTokens()){
-				if(text.indexOf(text.charAt(i)) == i){
-					tokenizer.nextToken();
-				}
-				if(text.indexOf(text.charAt(i)) != i){
-					println(tokenizer.nextToken());
-				}
+				println(tokenizer.nextToken());
 			}
 		}
 	}	
