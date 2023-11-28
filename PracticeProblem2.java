cb4064dd323fd23ae0bb086c715bb6c4abf4b537
S <s>
2023-11-28 17:59:00 +0400

    {"types":"C","runs":24}

--- PracticeProblem2.java
+++ PracticeProblem2.java
@@ -1,4 +1,18 @@
+import acm.program.ConsoleProgram;
 
-public class PracticeProblem2 {
-
+//2. კონსოლიდან შემოდის ტექსტი. დაბეჭდეთ True თუ ტექსტში არსებული ყველა lowercase ინგლისური ასო ზრდადობით არის დალაგებული, False თუ არაა.
+//ტექსტში სხვა სიმბოლოებიც შეიძლება შეგხვდეთ და იმათ ყურადღება არ მიაქციოთ
+public class PracticeProblem2 extends ConsoleProgram {
+	public void run(){
+		String text = readLine("Give me a text");
+		for(int i = 0 ; i < text.length(); i++){
+			char curChar =text.charAt(i);
+			char prevChar = text.charAt(i - 1);
+			if(curChar < prevChar){
+				println(false);
+				break;
+			}
+		}
+		println(true);
+	}
 }
