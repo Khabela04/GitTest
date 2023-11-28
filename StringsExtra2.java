eb03fa8cd6f9dbddea9ff681d4afe95e807f6b33
S <s>
2023-11-28 17:19:54 +0400

    {"types":"C","runs":1}

--- StringsExtra2.java
+++ StringsExtra2.java
@@ -1,4 +1,28 @@
+import acm.program.ConsoleProgram;
 
-public class StringsExtra2 {
+//კონსოლიდან შემოგდით ვალიდური რომაული რიცხვეის შესაბამისი სტრინგები, მანამ არ შემოგივათ სიტყვა “quit”. 
+//თითოეული რომაული რიცხვის სტრინგის წაკითხვის შემდეგ მომხმარებელს გამოუტანეთ ეგ რიცხვი intად.
 
-}
+public class StringsExtra2 extends ConsoleProgram {
+	public void run(){
+		String romanNum = readLine("Give me a Roman Number: ");
+		int number = 0;
+		for(int i = 0; i < romanNum.length(); i++){
+			char charecter = romanNum.charAt(i);
+			if(charecter == 'I'){
+				number += 1;
+			}
+			if(charecter == 'V'){
+				number += 5;
+			}
+			if(charecter == 'X'){
+				number += 10;
+			}
+		}
+		println(number);
+			
+			
+//			I, V, X
+		}
+	}
+
