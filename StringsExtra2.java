f9ec92cc46b8f4582d0de3991e28afda56ed3899
S <s>
2023-11-28 17:23:11 +0400

    {"types":"C","runs":4}

--- StringsExtra2.java
+++ StringsExtra2.java
@@ -10,7 +10,11 @@ public class StringsExtra2 extends ConsoleProgram {
 		for(int i = 0; i < romanNum.length(); i++){
 			char charecter = romanNum.charAt(i);
 			if(charecter == 'I'){
+				if(i < romanNum.indexOf('V') && i < romanNum.indexOf('X')){
+					number -= 1;
+				}else{
 				number += 1;
+				}
 			}
 			if(charecter == 'V'){
 				number += 5;
