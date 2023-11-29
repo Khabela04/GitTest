e69c5b163222574ca506710d561e3b023320e1e3
S <s>
2023-11-29 23:47:08 +0400

    {"types":"C","runs":8}

--- extra6_1.java
+++ extra6_1.java
@@ -1,4 +1,13 @@
+import acm.program.ConsoleProgram;
 
-public class extra6_1 {
-
+public class extra6_1 extends ConsoleProgram{
+	public void run(){
+		int number = 1;
+		int blank = 0;
+		while(number != 0){
+			number = readInt("Give me a number");
+			blank += number;
+		}
+		println(blank);
+	}
 }
