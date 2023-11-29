a8c622681129c0c953290f7e704b0ef8a1e1c76c
S <s>
2023-11-29 23:03:06 +0400

    {"types":"C","runs":1}

--- extra5_1.java
+++ extra5_1.java
@@ -1,4 +1,17 @@
+import java.awt.Color;
 
-public class extra5_1 {
+import acm.graphics.GLabel;
+import acm.program.GraphicsProgram;
 
+public class extra5_1 extends GraphicsProgram{
+	public void run(){
+		double x = getWidth();
+		double y = getHeight();
+		GLabel text = new GLabel("width: " + x + "height: " + y);
+		text.setFont("London");
+		text.setColor(Color.RED);
+		add(text, (getWidth() - text.getWidth())/2, getHeight()/2 + text.getHeight()/2);
+	}
 }
+
+
