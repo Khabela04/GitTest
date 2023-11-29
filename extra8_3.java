4c971b0a92dc09f7dd8e7f801329535324e43819
S <s>
2023-11-30 00:55:15 +0400

    {"types":"C","runs":33}

--- extra8_3.java
+++ extra8_3.java
@@ -6,7 +6,7 @@ public class extra8_3 extends ConsoleProgram{
 		println(fib(n));
 	}
 	private int fib(int n){
-		int prevNum = 1;
+		int prevNum = 0;
 		int nextNum = 0;
 		int curNum = 0;
 		for(int i = 0; i < n; i ++){
@@ -14,7 +14,7 @@ public class extra8_3 extends ConsoleProgram{
 			nextNum = curNum + prevNum;
 			prevNum = curNum;
 		}
-		return curNum;
+		return nextNum;
 // 	0, 1, 1, 2, 3, 5 ......
 	}
 }
