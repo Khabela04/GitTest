554f1e523080ba246e6b03afd5535262188a6f0c
S <s>
2023-11-30 00:51:50 +0400

    {"types":"C","runs":23}

--- extra8_3.java
+++ extra8_3.java
@@ -1,4 +1,19 @@
+import acm.program.ConsoleProgram;
 
-public class extra8_3 {
-
+public class extra8_3 extends ConsoleProgram{
+	public void run(){
+		int n = readInt("Which number of fibonacci do you want: ");
+		println(fib(n));
+	}
+	private int fib(int n){
+		int prevNum = 1;
+		int nextNum = 0;
+		for(int i = 0; i < n; i ++){
+			int curNum = i;
+			nextNum = curNum + prevNum;
+			prevNum = curNum;
+		}
+		return nextNum;
+// 	0, 1, 1, 2, 3, 5 ......
+	}
 }
