4ef241929bec809a3cce64ab0d1a23dd3d569a5f
S <s>
2023-11-30 01:13:21 +0400

    {"types":"C","runs":50}

--- extra8_3.java
+++ extra8_3.java
@@ -1,9 +1,20 @@
+import acm.graphics.GRect;
 import acm.program.ConsoleProgram;
+import acm.program.GraphicsProgram;
 
-public class extra8_3 extends ConsoleProgram{
+public class extra8_3 extends GraphicsProgram{
 	public void run(){
-		int n = readInt("Which number of fibonacci do you want: ");
-		println(n+"th number in fibonacci is " +fib(n));
+		int n = 5;
+		addSquares(10*fib(n), 0, 10 );
+	}
+	private void addSquares(int length, int y, int border) {
+		for(int i = 0; i < 5; i++){
+			GRect square = new GRect(length, length);
+			int x = 0;
+			add(square, x, y);
+			x += border;
+		}
+		
 	}
 	private int fib(int n){
 		int prevNum = 1;
