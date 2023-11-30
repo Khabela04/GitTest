42671267cd730ddd5e06f8bdda1aef6757319296
S <s>
2023-12-01 02:14:58 +0400

    {"types":"C","runs":39}

--- midterm_pr4.java
+++ midterm_pr4.java
@@ -32,7 +32,7 @@ public class midterm_pr4 extends ConsoleProgram{
 				blank += " ";
 			}
 		}
-		if(text1.equals(blank)){
+		if(text1==blank){
 			println("This words are anagrams");
 		}else{
 			println("This words aren't anagrams");
