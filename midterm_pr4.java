4d7873cb21110a580ee9ce2172f26547bc06e1ec
S <s>
2023-12-01 01:56:30 +0400

    {"types":"C","runs":21}

--- midterm_pr4.java
+++ midterm_pr4.java
@@ -1,4 +1,29 @@
+import java.util.StringTokenizer;
 
-public class midterm_pr4 {
+import acm.program.ConsoleProgram;
 
+//4. დაწერეთ მეთოდი რომელსაც გადაეცემა ინგლისური ტექსტი, და აბრუნებს ტექსტს, რომელიც არის საწყისი ტექსტი შეცვლილი ისე, რომ თითოეული სიყვა არის შებრუნებული.  
+//კონსოლიდან შემოიყვანეთ ტექსტი და დაბეჭდეთ ამ მეთოდის მიერ დაბრუნებული შედეგი.
+//ტექსტი შედგება მხოლოდ შემდეგი სიმბოლოებისგან: ინგლისური ასოები, მძიმე, წერტილი, space. (Practice04)
+
+
+
+public class midterm_pr4 extends ConsoleProgram{
+	public void run(){
+		String text = readLine("Give me a text");
+		StringTokenizer tokens = new StringTokenizer(text, " ,.");
+		String blank = "";
+		while(tokens.hasMoreTokens()){
+			String word = tokens.nextToken();
+			for(int i = 0; i < word.length(); i++){
+				char charecter = word.charAt(word.length() - i - 1);
+				blank += charecter;
+			}
+			if(tokens.hasMoreTokens()){
+				blank += " ";
+			}
+		}
+		println(blank);
+	}
 }
+
