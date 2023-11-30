28152b7a31c6970353e15773ccf65f7018aad332
S <s>
2023-12-01 02:49:13 +0400

    {"types":"C","runs":57}

--- midterm_pr4.java
+++ midterm_pr4.java
@@ -14,12 +14,9 @@ public class midterm_pr4 extends ConsoleProgram{
 		String text = readLine("Give me a text");
 		String text1 = readLine("Give me your guess which word me be anagram of this ");
 		boolean indicator = false;
-		int counter = 0;
 		StringTokenizer tokens = new StringTokenizer(text);
-		StringTokenizer tokenson = new StringTokenizer(text1);
 		char n = 0;
 		while(tokens.hasMoreTokens()){
-			String word1 = tokenson.nextToken();
 			String word = tokens.nextToken();
 			String word2 = blank;
 			for(int i = 0; i < word.length(); i++){
@@ -27,7 +24,6 @@ public class midterm_pr4 extends ConsoleProgram{
 				char charecter1 = blank.charAt(i+1);
 				char charecter2 = word2.charAt(i+1);
 				if(charecter1 != charecter2){
-					counter++;
 				}
 
 				if(charecter == ',' || charecter == '.'){
@@ -46,9 +42,6 @@ public class midterm_pr4 extends ConsoleProgram{
 			if(tokens.hasMoreTokens()){
 				blank += " ";
 			}
-		}
-		if(counter > 1){
-			println("there's no chance for change");
 		}
 		
 		if(text1.equals(blank)){
