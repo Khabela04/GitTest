562763583c807a3a25de13c3a794c88876dc63fc
S <s>
2023-11-30 17:36:59 +0400

    {"types":"C","runs":8}

--- Midterm_prac.java
+++ Midterm_prac.java
@@ -1,3 +1,6 @@
+import java.awt.event.MouseEvent;
+
+import acm.graphics.GObject;
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
@@ -10,18 +13,46 @@ public class Midterm_prac extends GraphicsProgram {
 	private static final int RADIUS_MAX = 50;
 	private static final int V_MIN = 1;
 	private static final int V_MAX = 5;
-	RandomGenerator rgen = RandomGenerator.getInstance();
+	private static final int DELAY = 100;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	private GOval circle;
+	private int v_x = 0;
+	private int v_y = 0;
 	public void run(){
 		addMouseListeners();
 		addRandomCircle();
+		while(true){
+			circle.move(v_x, v_y);
+			pause(DELAY);
+		}
+		
 	}
 	private void addRandomCircle(){
 		double radius = rgen.nextDouble(RADIUS_MIN, RADIUS_MAX);
-		GOval circle = new GOval(2 * radius,2 * radius);
+		circle = new GOval(2 * radius,2 * radius);
 		circle.setFilled(true);
 		circle.setColor(rgen.nextColor());
 		double x = rgen.nextDouble(0, getWidth() - 2 * radius);
 		double y = rgen.nextDouble(0, getHeight() - 2 * radius);
 		add(circle, x, y);
 	}
+	@Override
+	public void mouseClicked(MouseEvent e){
+		GObject obj = getElementAt(e.getX(), e.getY());
+		if(obj != null){
+			v_x = getRandomSpeed();
+			v_y = getRandomSpeed();
+		}
+		
+	}
+	private int getRandomSpeed(){
+		int v = rgen.nextInt(V_MIN, V_MAX);
+		boolean negSpeed = rgen.nextBoolean();
+		if(negSpeed){
+			v = -v;
+		}
+		return v;
+	}
+	
+	
 }
