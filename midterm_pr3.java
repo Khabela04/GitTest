4466cb9beb6f5927576dc284189d93c3a548f11c
S <s>
2023-12-01 01:44:37 +0400

    {"types":"C","runs":14}

--- midterm_pr3.java
+++ midterm_pr3.java
@@ -1,4 +1,30 @@
+import java.util.StringTokenizer;
 
-public class midterm_pr3 {
+import acm.program.ConsoleProgram;
 
+//3. დაწერეთ მეთოდი რომელსაც გადაეცემა ინგლისური ტექსტი, და აბრუნებს ტექსტს, რომელიც არის საწყისი ტექსტი შეცვლილი ისე, 
+//რომ ყველა სიყვა დიდი ასოთი იწყებოდეს და სიტყვაში ყოველი შემდგომი ასო იყოს პატარა.  კონსოლიდან შემოიყვანეთ ტექსტი და 
+//დაბეჭდეთ ამ მეთოდის მიერ დაბრუნებული შედეგი. 
+//მაგ: “i have A pEN.” -> “I Have A Pen” (Practice03)
+//Tags: String, Tokenizer, toLowerCase, toUpperCase
+
+
+public class midterm_pr3 extends ConsoleProgram{
+	public void run(){
+		String text = readLine("Give me a text: ");  // saba magaria  
+		String blank = "";
+		StringTokenizer tokens = new StringTokenizer(text);
+		while(tokens.hasMoreTokens()){
+			for(int i = 0; i < tokens.nextToken().length(); i++){
+				if(i == 0){
+					String n = ""+tokens.nextToken().charAt(0);
+					blank += (""+tokens.nextToken().charAt(0)).toUpperCase();
+				}else{
+					blank += (""+tokens.nextToken().charAt(i)).toLowerCase();
+				}
+			}
+		}
+		println(blank);
+	}
 }
+
