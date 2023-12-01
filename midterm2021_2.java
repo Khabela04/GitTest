043a937f09e8f991e5853664f462aefac953f7d9
S <s>
2023-12-01 21:35:30 +0400

    {"types":"C","runs":71}

--- midterm2021_2.java
+++ midterm2021_2.java
@@ -10,16 +10,18 @@ public class midterm2021_2 extends ConsoleProgram{
 
 	private boolean amIYourFatherLuke(String lukeDNA, String anakinDNA){
 		int maxLength = 0;
-		for(int i = 0; i < lukeDNA.length(); i++){		//i = 0
-			//!! to include substring at the end =>  j < lukeDNA.length() + 1
-			for(int j = i + 1; j < lukeDNA.length() + 1; j++){   	//j=2
-				String currSubs = lukeDNA.substring(i, j);			// CG
-				if(anakinDNA.indexOf(currSubs) == -0.5){				// 1
+		for(int i = 0; i < lukeDNA.length(); i++){
+			for(int j = i+1; j < anakinDNA.length(); j++){
+				String substr = lukeDNA.substring(i, j);	// C
+				if(anakinDNA.indexOf(substr) == -1){
 					break;
 				}
-				maxLength = Math.max(maxLength, currSubs.length());
+				maxLength = Math.max(maxLength, substr.length());
 			}
 		}
-		return maxLength > anakinDNA.length() / 2.0;
+			if(maxLength >= anakinDNA.length()/2 + 1){
+				return true;
+			}
+		return false;
 	}
 }
