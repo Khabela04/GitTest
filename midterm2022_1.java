b856115fe454584053dfde9648f16ccbc86c6b01
S <s>
2023-12-01 22:58:25 +0400

    {"types":"C","runs":104}

--- midterm2022_1.java
+++ midterm2022_1.java
@@ -3,7 +3,7 @@ import acm.program.ConsoleProgram;
 public class midterm2022_1 extends ConsoleProgram{
 	public void run(){
 		String text1 = "iyo arabets rostevan";
-		String text2 = "iyo rets rostevan";
+		String text2 = "iyo raets rostevan";
 		println(manipulation(text1, text2));
 	}
 	private boolean manipulation(String text1, String text2){
