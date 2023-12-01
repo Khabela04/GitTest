0ab7486719aa150ea9b044ffa05eff58fe625752
S <s>
2023-12-02 02:57:01 +0400

    {"types":"C","runs":111}

--- midterm2017_3.java
+++ midterm2017_3.java
@@ -4,12 +4,18 @@ public class midterm2017_3 extends ConsoleProgram{
 	public void run(){
 		String text1 = "gamarjoba";
 		String text2 = "baro";
+		boolean indicator = true;
 		for(int i = 0; i < text2.length(); i++){
 			char letter2 = text2.charAt(i);
 				if(text1.indexOf(letter2) == -1){
-					println("can't do");
+					indicator = false;
 					break;
 				}
+		}
+		if(indicator){
+			println("You can do it");
+		}else{
+			println("You can't do it");
 		}
 	}
 }
