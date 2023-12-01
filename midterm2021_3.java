bc6dcb7ddf967870d65f30fab5cbf33c89fbffbf
S <s>
2023-12-01 22:03:38 +0400

    {"types":"C","runs":90}

--- midterm2021_3.java
+++ midterm2021_3.java
@@ -7,26 +7,24 @@ import acm.program.GraphicsProgram;
 
 
 public class midterm2021_3 extends GraphicsProgram{
-	private GLine point;
-	double xStart = 0;
-	double yStart = 0;
-	public void run(){
-		addMouseListeners();
+		GLine point;
+		double xStart = 0;
+		double yStart = 0;
+		public void run(){
+			addMouseListeners();
+			drawLine();
+		}
+		private void drawLine(){
+			point = new GLine(xStart, yStart, 0, 0);
+			add(point);
+			
+		}
+		public void mouseDragged(MouseEvent e){
+			point.setEndPoint(e.getX(), e.getY());
+		}
+		public void mouseClicked(MouseEvent e){
+			point.setEndPoint(e.getX(), e.getY());
+			xStart = e.getX();
+			yStart = e.getY();
+		}
 	}
-//	private void drawLine(){
-//		point = new GLine(xStart, yStart, 0, 0);
-//		add(point);
-//		
-//	}
-	@Override
-	public void mouseDragged(MouseEvent e){
-		point.setEndPoint(e.getX(), e.getY());
-	}
-	@Override
-	public void mouseClicked(MouseEvent e){
-		point = new GLine(e.getX(), e.getY(),e.getX() , e.getY());
-		add(point);
-//		xStart = e.getX();
-//		yStart = e.getY();
-	}
-}
