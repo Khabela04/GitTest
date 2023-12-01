05922e2c1bfb71e7be1fa74015b408bdcd4ce52d
S <s>
2023-12-02 02:55:28 +0400

    {"types":"C","runs":109}

--- midterm2017_3.java
+++ midterm2017_3.java
@@ -1,4 +1,15 @@
+import acm.program.ConsoleProgram;
 
-public class midterm2017_3 {
-
+public class midterm2017_3 extends ConsoleProgram{
+	public void run(){
+		String text1 = "gamarjoba";
+		String text2 = "baro";
+		for(int i = 0; i < text2.length(); i++){
+			char letter2 = text2.charAt(i);
+				if(text1.indexOf(letter2) == -1){
+					println("can't do");
+					break;
+				}
+		}
+	}
 }
