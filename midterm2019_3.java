61de7904332227f631e0a1e3d91b151d44d0399e
S <s>
2023-12-02 00:24:44 +0400

    {"types":"C","runs":10}

--- midterm2019_3.java
+++ midterm2019_3.java
@@ -1,4 +1,51 @@
+import java.awt.Color;
+import java.awt.event.MouseEvent;
 
-public class midterm2019_3 {
+import acm.graphics.GObject;
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
+public class midterm2019_3 extends GraphicsProgram{
+	private final int CIRCLE_D = 100;
+	private final int DELAY = 100;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	private GObject selectedObject = null;
+	public void run(){
+		addMouseListeners();
+	}
+	public void mouseClicked(MouseEvent e){
+		GObject obj = getElementAt(getX(), getY());
+		if(obj == null){
+			GOval circle = new GOval(CIRCLE_D, CIRCLE_D);
+			add(circle, e.getX() - CIRCLE_D/2, e.getY() -CIRCLE_D/2);
+			circle.setFilled(true);
+			circle.setColor(rgen.nextColor());
+		}else{
+			while(true){
+				int number = rgen.nextInt(1,5);
+				if(number == 1){
+					obj.setColor(Color.GREEN);
+				}
+				if(number == 2){
+					obj.setColor(Color.RED);
+				}
+				if(number == 3){
+					obj.setColor(Color.BLUE);
+				}
+				if(number == 4){
+					obj.setColor(Color.BLACK);
+				}
+				if(number == 5){
+					obj.setColor(Color.YELLOW);
+				}
+				pause(DELAY);
+				if(obj.getColor() == Color.GREEN){
+					break;
+				}
+			}
+		
+		}
+	}
 }
+
