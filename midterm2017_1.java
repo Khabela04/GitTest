f20fb6a16766e623c2ba5cf12d51546234443ef5
S <s>
2023-12-02 01:32:29 +0400

    {"types":"C","runs":44}

--- midterm2017_1.java
+++ midterm2017_1.java
@@ -39,17 +39,17 @@ public class midterm2017_1 extends GraphicsProgram{
 		point = new GPoint(x1, y1);
 	}
 	public void mouseDragged(MouseEvent e){
-		if(getElementAt(getX(), getY()) != null){
-			if(getElementAt(x1, y1) != null){
-				if(point.getX() > e.getX()){
+//		if(getElementAt(getX(), getY()) != null){
+//			if(getElementAt(x1, y1) != null){
+//				if(point.getX() > e.getX()){
 					n--;
 					add(text, (getWidth() - text.getWidth())/2, getHeight()/2 + text.getHeight()/2);
-				}
-				if(point.getX() < e.getX() ){
-					n++;
-					add(text, (getWidth() - text.getWidth())/2, getHeight()/2 + text.getHeight()/2);
-				}
-			}
-		}
+//				}
+//				if(point.getX() < e.getX() ){
+//					n++;
+//					add(text, (getWidth() - text.getWidth())/2, getHeight()/2 + text.getHeight()/2);
+//				}
+//			}
+//		}
 	}
 }
