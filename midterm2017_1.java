d8a75601c4cc75d29ae86fa9572da7ef1115268f
S <s>
2023-12-02 01:21:14 +0400

    {"types":"C","runs":29}

--- midterm2017_1.java
+++ midterm2017_1.java
@@ -1,4 +1,49 @@
+import java.awt.event.MouseEvent;
 
-public class midterm2017_1 {
+import acm.graphics.GLabel;
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
+/*განჯრის ცენტრში მოთავსებულია ჩარჩო, რომელშიც ნაჩვენებია ქულის რიცხვითი მნიშვნელობა. ჩარჩო კვადრატული ფორმისაა,
+ მისი ზომა მოცემულია BOX_SIZE კონსტანტის საშუალებით და იგი განთავსებულია ფანჯრის ცენტრში. 
+ ქულის რიცხვითი მნიშვნელობა წარმოადგენს GLabel-ს და ისიც ფანჯრის ცენტშია განთავსებული.
+გარდა ვიზუალური ნაწილისა პროგრამა უნდა აკმაყოფილებდეს შემდეგ მოთხოვნებს:
+ჩარჩოს შიგნით მარჯვნიდან მარცხნივ მაუსის დრაგზე, ქულის მნიშნელობა უნდა მცირდებოდეს ერთით. თუ მნიშვნელობა 0-ია მაშინ იგი უცვლელი უნდა რჩებოდეს.
+ჩარჩოს შიგნით, მარცხნიდან მარჯვნივ დრაგზე კი, ქულის მნიშვნელობა უნდა იზრდებოდეს ერთით. 
+თუ ქულის მნიშვნელობა 9-ის ტოლია მაშინ იგი უცვლელი უნდა დარჩეს.
+შენიშვნა: დავუშვათ მაუსის ღილაკზე დაჭერის მომენტში მისი ლოკაცია იყო (x1, y1), ხოლო აშვების მომენტში (x2, y2). 
+თუ ორივე წერტილი მდებარეობს ჩარჩოს შიგნით და x1 > x2 ზე მაშინ მაუსის ეს მოძრაობა შეესაბამება მარჯვნიდან მარცხნივ დრაგს. 
+თუ x1 < x2 ზე მაშინ პირიქით, მარცხნიდან მარჯვნივ დრაგს.
+*/
+
+
+
+public class midterm2017_1 extends GraphicsProgram{
+	private static final int BOX_SIZE = 50;
+	double x1 = 0;
+	double y1 = 0;
+	int n = 0;
+	public void run(){
+		addMouseListeners();
+		GRect square = new GRect(BOX_SIZE, BOX_SIZE);
+		add(square, (getWidth() - BOX_SIZE)/2, (getHeight() - BOX_SIZE)/2);
+		GLabel text = new GLabel(""+n);
+		add(text, (getWidth() - text.getWidth())/2, getHeight()/2 + text.getHeight()/2);
+	}
+	public void mouseClicked(MouseEvent e){
+		x1 = e.getX();
+		y1 = e.getY();
+	}
+	public void mouseDrageed(MouseEvent e){
+		if(getElementAt(getX(), getY()) != null){
+			if(getElementAt(x1, y1) != null){
+				if(x1 > e.getX()){
+					n--;
+				}
+				if(x1 < e.getX() ){
+					n++;
+				}
+			}
+		}
+	}
 }
