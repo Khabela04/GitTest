9fd368143442edc9cca739314852b32b235140e7
S <s>
2023-12-02 02:45:40 +0400

    {"types":"C","runs":102}

--- midterm2017_2.java
+++ midterm2017_2.java
@@ -1,4 +1,25 @@
+import acm.program.ConsoleProgram;
 
-public class midterm2017_2 {
-
+public class midterm2017_2 extends ConsoleProgram{
+	public void run(){
+		int number = readInt("Give me a number");
+		int maximum = 0;
+		for(int i = 0; i < number; i++){
+			if(isPrime(i)){
+				if(number % i == 0){
+					if(i > maximum){
+						maximum = i;
+					}
+				}
+			}
+		}
+	}
+	private boolean isPrime(int i){
+		for(int j = 2; j < i; j++){
+			if(i % j == 0){
+				return false;
+			}
+		}
+		return true;
+	}
 }
