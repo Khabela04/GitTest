112b27786b833be02d766289c048b2794fda6a69
S <s>
2023-12-01 22:57:16 +0400

    {"types":"C","runs":102}

--- midterm2022_1.java
+++ midterm2022_1.java
@@ -1,4 +1,35 @@
+import acm.program.ConsoleProgram;
 
-public class midterm2022_1 {
-
+public class midterm2022_1 extends ConsoleProgram{
+	public void run(){
+		String text1 = "iyo arabets rostevan";
+		String text2 = "iyo rets rostevan";
+		println(manipulation(text1, text2));
+	}
+	private boolean manipulation(String text1, String text2){
+		for(int i = 1; i < text1.length(); i++){
+			String part1 = text1.substring(0,i);	// i
+			String part2 = text1.substring(i);		// yo arabets rostevan
+			for(int j = 0; j < part1.length(); j++){
+				String subPart1 = subber(part1, part1.charAt(j));
+				for(int n = 0; n < part2.length(); n++){
+					String subPart2 = subber(part2, part2.charAt(i));
+					String modText1 = subPart1 + subPart2;
+					if(modText1.equals(text2)){
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	private String subber(String part1, char charAt) {
+		String blank = "";
+		for(int i = 0; i < part1.length(); i++){
+			if(part1.charAt(i) != charAt){
+				blank += part1.charAt(i);
+			}
+		}
+		return blank;
+	}
 }
