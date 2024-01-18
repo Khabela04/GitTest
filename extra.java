f2b2c5a9d4782ffe8c5698ec4c2a28dad73e5787
S <s>
2024-01-19 00:24:11 +0400

    {"types":"C","runs":10}

--- extra.java
+++ extra.java
@@ -24,12 +24,13 @@ public class extra extends ConsoleProgram {
 	public void eachCons(int [] x, int n){
 		int l = x.length - n +1;
 		int [][] result = new int[l][n];
-		for(int i = 0; i < l-1; i++){
+		for(int i = 0; i < l; i++){
 			int[] blank = new int[n];
 			for(int j = 0; j < n; j++){
 				result[i][j] = x[i+j];
 			}
 		}
+		println("saba" instanceof String);
 		toString(result);
 	}
 	private void toString(int[][] result) {
