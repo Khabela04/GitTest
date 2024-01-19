f44fb68a4e8706bd0507fa66f95f4a2e9d402f8d
S <s>
2024-01-20 01:50:01 +0400

    {"types":"C","runs":48}

--- Problem62.java
+++ Problem62.java
@@ -9,7 +9,7 @@ public class Problem62 extends ConsoleProgram {
 	public void run(){
 		String name = readLine("Name: ");
 		names = new ArrayList <String>();
-		while(name!= "s"){
+		while(!(name.equals(""))){
 			name = readLine("Name: ");
 			names.add(name);
 		}
