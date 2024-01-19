011f1e486c80e03ede6b5acc1162bb496c1f1fd0
S <s>
2024-01-20 02:02:13 +0400

    {"types":"C","runs":56}

--- Problem62.java
+++ Problem62.java
@@ -19,15 +19,14 @@ public class Problem62 extends ConsoleProgram {
 
 	private void printer(String name) {
 		int counter = 0;
+		int name1 = 0;
 		for(int i = 0; i < names.size();i++){
 			for(int j = 0; j < names.size(); j++){
-				if((j>i) &&(names.get(i).equals(names.get(j)))){
+				if((names.get(i).equals(names.get(j)))){
 					counter++;
 				}
 			}
-			if(counter > 0){
-				println(names.get(i) + " " + counter);
-			}
+			println(names.get(i) + " " + counter);
 			counter = 0;
 		}
 	}
